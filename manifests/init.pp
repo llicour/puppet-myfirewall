a291413745fc73ac2d7a1c7e824ed9cb6fecbfa7
Laurent Licour <laurent@licour.com>
2013-06-09 11:37:46 +0200

    iptables flush is handled by puppet firewall provider. Adding manual flush just add errors iat the 1st run

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,19 +2,6 @@
 
 class myfirewall {
 
-  $ipv4_file = $::osfamily ? {
-      'Debian' => '/etc/iptables/rules.v4',
-      default  => '/etc/sysconfig/iptables',
-  }
-
-  exec { 'purge-default-firewall' :
-      command =>
-        "/sbin/iptables -F && /sbin/iptables-save > ${ipv4_file} && /sbin/service iptables restart",
-      onlyif  =>
-        "/bin/grep -q \"Firewall configuration written by\" ${ipv4_file}",
-      user    => 'root',
-  }
-
   # prevent shutdown hanging at "iptables unloading modules"
   file { '/etc/sysconfig/iptables-config' :
       ensure => present,
@@ -28,13 +15,11 @@ class myfirewall {
 
   Firewall {
         before  => Class[ 'myfirewall::post' ],
-        require => [  Class[ 'myfirewall::pre' ],
-                      Exec[ 'purge-default-firewall' ], ],
+        require => Class[ 'myfirewall::pre' ],
   }
 
   class { ['myfirewall::pre', 'myfirewall::post']: }
 
-  #class { 'firewall': }
   include firewall
 
 }
